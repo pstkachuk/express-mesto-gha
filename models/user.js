14e1d0d9aee22d1f5af23b378dbe011773196aa2
Pavel Tkachuk <p.s.tkachuk@yandex.ru>
2022-08-10 22:18:38 +0300

    feat: создать схемы карточки и пользователя

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlenght: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlenght: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('use', userSchema);
